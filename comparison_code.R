cf07f2f712b27dcfd68c8301ad62887a05f6ea51
Mauno Joukamaa <44747647+mjoukamaa@users.noreply.github.com>
2018-11-25 12:36:15 +0200

    Update comparison_code.R

--- comparison_code.R
+++ comparison_code.R
@@ -1,18 +1,24 @@
+# Loading up the tidyverse library
+# for easier data handling
 library(tidyverse)
 
+# Setting up a locale variable for read_csv2() to handle
+# character encodings and other features of the data
 election_data_settings <- locale(date_names = "fi", date_format = "%AD",
-                              time_format = "%AT", decimal_mark = ",",
-                              grouping_mark = " ", tz = "EET", 
-                              ncoding = "ISO-8859-1", asciify = FALSE)
-                              
+                                 time_format = "%AT", decimal_mark = ",",
+                                 grouping_mark = " ", tz = "EET", 
+                                 encoding = "ISO-8859-1", asciify = FALSE)
+
+# Reading up the different .csv datafiles into tibbles/dataframes
 parliamentary_by_candidate <- read_csv2('e-2015_ehd_01.csv', col_names = FALSE,
-                                    locale = election_data_settings)
+                                        locale = election_data_settings)
                                     
 parliamentary_by_area <- read_csv2('e-2015_alu_01.csv', col_names = FALSE,
-                                  locale = election_data_settings)
+                                   locale = election_data_settings)
                                   
 municipal_by_candidate <- read_csv2('kv-2017_teat_maa.csv', col_names = FALSE,
-                                     n_max = 181000, locale = election_data_settings)
+                                    n_max = 181000, locale = election_data_settings)
 
 municipal_by_area <- read_csv2('kv-2017_taat_maa.csv', col_names = FALSE,
                                locale = election_data_settings)
+
