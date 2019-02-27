996263473857807d6db3dcaf149f24919d99f7f8
Mauno Joukamaa <mauno.joukamaa@gmail.com>
2019-02-27 18:13:26 +0200

    Update comparison_code.R

--- comparison_code.R
+++ comparison_code.R
@@ -14,15 +14,22 @@ election_data_settings <- locale(date_names = "fi", date_format = "%AD",
 # Reading up the different .csv datafiles into tibbles/dataframes,
 # using read_csv2 since fields are separated by semicolons
 
+# The first two .csv files include data only from
+# Helsinki voting district (district number 01)
 parliamentary_by_candidate_raw <- read_csv2('e-2015_ehd_01.csv', col_names = FALSE,
-                                        locale = election_data_settings)
+                                            locale = election_data_settings)
                                     
-parliamentary_by_area_raw <- read_csv2('e-2015_alu_01.csv', col_names = FALSE,
-                                   locale = election_data_settings)
-                                  
-municipal_by_candidate_raw <- read_csv2('kv-2017_teat_maa.csv', col_names = FALSE,
-                                    n_max = 181000, locale = election_data_settings)
+parliamentary_by_area_raw <- read_csv2('e-2015_alu_01.csv',col_names = FALSE,
+                                       locale = election_data_settings)
 
+# As the next file includes candidate vote totals by individual voting areas
+# for the whole country, n_max is used to read only the records from Helsinki
+# voting district in order to keep the data file size in reasonable limits
+municipal_by_candidate_raw <- read_csv2('kv-2017_teat_maa.csv', col_names = FALSE,
+                                    n_max = 181028, locale = election_data_settings)
+
+# This file covers also the whole country, but can be read in whole due to its
+# smaller size; records from other municipalities are filtered out later
 municipal_by_area_raw <- read_csv2('kv-2017_taat_maa.csv', col_names = FALSE,
                                locale = election_data_settings)
 
@@ -57,6 +64,8 @@ parliamentary_combined <- left_join(parliamentary_candidate_data,
 # Making the corresponding selections, conversions and joins
 # for the municipal elections data
 municipal_by_area <- municipal_by_area_raw %>%
+  # filtering out data from other municipalities than Helsinki
+  filter(X6 == 'HEL') %>% 
   select(area_name = X10, area_number = X5, area_vote_total = X66) %>%
   mutate(area_number = substr(area_number, 1, nchar(area_number)-1),
          area_vote_total = as.integer(area_vote_total))
